24154f2777a2ae4643a22d054a7cba89ac496113
smp2070 <shvepsik7@gmail.com>
2019-04-03 17:19:20 +0300

    added redux features

--- src/index.js
+++ src/index.js
@@ -3,21 +3,52 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import firebase from './firebase';
 
 import 'semantic-ui-css/semantic.min.css';
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
 
-import './firebase';
+import { createStore, bindActionCreators } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+import { setUser } from './actions';
 
-const Root = () => (
-    <Router>
-        <Switch>
-            <Route exact path="/" component={App} />
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-        </Switch>
-    </Router>
-);
+const store = createStore(rootReducer, composeWithDevTools())
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+
+class Root extends React.Component {
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                this.props.setUser(user);
+                this.props.history.push("/");
+            }
+        })
+    }
+    render() {
+        return (
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+            </Switch>
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    isLoading: state.user.isLoading,
+    
+});
+const mapDispatchToProps = dispatch => bindActionCreators({ setUser }, dispatch);
+
+const RootWithAuth = withRouter(connect(mapStateToProps, mapDispatchToProps)(Root));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <RootWithAuth />
+        </Router>
+    </Provider>, document.getElementById('root'));
