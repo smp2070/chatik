b68ce2e7042b1ce50010cef7504de7ba13ae4f0e
smp2070 <shvepsik7@gmail.com>
2019-04-06 20:35:37 +0300

    Channels.jsx

--- src/components/SidePanel/SidePanel.jsx
+++ src/components/SidePanel/SidePanel.jsx
@@ -1,21 +1,30 @@
-import React, { Component } from 'react';
-import { Menu } from 'semantic-ui-react';
-import Channels from './Channels';
-import UserPanel from './UserPanel';
+import React from "react";
+import { Menu } from "semantic-ui-react";
 
-export default class SidePanel extends Component {
+import UserPanel from "./UserPanel";
+import Channels from "./Channels";
+import DirectMessages from "./DirectMessages";
+import Starred from "./Starred";
+
+class SidePanel extends React.Component {
   render() {
+    const { currentUser, primaryColor } = this.props;
+
     return (
-        <Menu
-            size="large"
-            inverted
-            fixed="left"
-            vertical
-            style={{ background: '#4c3c4c', fontSize: '1.2rem' }}
-            >
-            <UserPanel/>
-            <Channels/>
-        </Menu>
-    )
+      <Menu
+        size="large"
+        inverted
+        fixed="left"
+        vertical
+        style={{ background: primaryColor, fontSize: "1.2rem" }}
+      >
+        <UserPanel primaryColor={primaryColor} currentUser={currentUser} />
+        <Starred currentUser={currentUser} />
+        <Channels currentUser={currentUser} />
+        <DirectMessages currentUser={currentUser} />
+      </Menu>
+    );
   }
 }
+
+export default SidePanel;
